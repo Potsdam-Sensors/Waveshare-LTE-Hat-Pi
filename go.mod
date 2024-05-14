531c89ae3ce84ecd3e8096af0d9cb8c86e23cc61
Work <work@Brandons-MacBook-Pro.local>
2024-05-14 11:07:04 -0400

    updated main for lib

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Potsdam-Sensors/waveshare-lte-hat-pi
 
 go 1.21.5
+
+require github.com/Potsdam-Sensors/waveshare-lte-hat-pi/wavesharecomm v0.0.0-20240510194031-3ffedd1bf6dc
+
+require (
+	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Potsdam-Sensors/waveshare-lte-hat-pi/wavesharecomm"
+)
+
+func main() {
+	port, err := wavesharecomm.OpenWaveshareHatSerialPort()
+	if err != nil {
+		log.Fatalf("failed to open port: %v", err)
+	}
+	defer port.Close()
+
+	resp, ok, err := wavesharecomm.ExecuteCommand(port, wavesharecomm.CmdStart, time.Second)
+	log.Printf("Response: %v (%s)", resp, resp)
+	log.Printf("OK?: %v", ok)
+	log.Printf("Err: %v", err)
+
+	resp, ok, err = wavesharecomm.ExecuteCommandRead(port, wavesharecomm.CmdNetworkOperator, time.Second)
+	log.Printf("Response: %v (%s)", resp, resp)
+	log.Printf("OK?: %v", ok)
+	if err != nil {
+		log.Fatalf("Error executing read command %s: %v", wavesharecomm.CmdNetworkOperator, err)
+	}
+}
